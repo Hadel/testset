d91d3cac1fae4d1728204023df141551fb03a3fd
Jorden Verwer <jorden.verwer@goabout.com>
2013-09-05 12:06:12 +0200

    Update (deprecated) PS script

--- scripts/update_merge.ps1
+++ scripts/update_merge.ps1
@@ -1,9 +1,9 @@
 ni results -type directory -Force
 rm results\* -Recurse -Force
-foreach ($x in "1a","1g","2a1","2a2","2b","2c","2d","2e1","2e2","2e3","2e4","2e5","2f","3b","3c","3d","3e","3f","3g1","3g2")
+foreach ($x in "1a","1g","2a1","2a2","2b","2c","2d","2e1","2e2","2e3","2e4","2f","3b","3c","3d","3e","3f","3g1","3g2","3i","plannerstack_scenario")
 {
-	cd $x
-	7z a ..\results\$x.zip *txt
-	cd ..
+	cd $x\gtfs
+	7z a ..\..\results\$x.zip *txt
+	cd ..\..
 }
-java -jar ../../bin/onebusaway-gtfs-transformer-cli.jar --overwriteDuplicates results\*zip alltests.zip
+java -jar ../bin/onebusaway-gtfs-transformer-cli.jar --overwriteDuplicates results\*zip alltests.zip
